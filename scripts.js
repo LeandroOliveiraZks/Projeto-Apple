3c1c6f213325117c38be0b307ec178eb5ca100aa
LeandroOliveiraZks <oleandro2004@gmail.com>
2023-03-19 21:37:58 -0300

    Projeto Apple

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function MudaCor (cor) {
+    let CirculoDeCor = document . querySelector (".CirculoDeCor")
+    CirculoDeCor.style.background = cor
+}
+
+function MudaImagem (imagem) {
+    let imgiphones = document . querySelector(".iphones")
+    imgiphones.src = imagem
+}
